0b9225cf35c06c46b3435e97ac0cbad2983176f3
Mario Cordoba <mario.cordoba@astrogrp.com.ni>
2022-08-25 08:25:04 -0600

    Agregando los cambios

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,19 @@ namespace ConsoleApp1
 {
     class Program
     {
+        
+        public static void imp()
+        {
+            Console.WriteLine("Imprimiendo este cuadro");      
+        }
+
         static void Main(string[] args)
         {
+            //Hemos creado el repositorio
+
+            //Ahora vamos a guardar los cambios
+
+            imp();
         }
     }
 }
